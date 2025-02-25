8f10a9c38b309e7ea2b8a3bca4fb3d06d5db4902
sakirqa <sakir.qa@gmail.com>
2025-02-25 21:28:03 +0530

    Update the assertion

--- cypress/e2e/kgisl.cy.js
+++ cypress/e2e/kgisl.cy.js
@@ -17,12 +17,15 @@ describe("KGiSL assessment", () => {
       .should("eq", "India");
 
     //Enter weight 1
-    cy.get('input[placeholder="eg. 0.1kg"]').click({ force: true }).type("1");
+    cy.get('input[placeholder="eg. 0.1kg"]').click({ force: true }).type("1")
 
     //Click on Calculate
     cy.contains(" Calculate ").click({ force: true });
 
     //Assert that multiple quotes return
-    cy.get("div.border-b.border-gray-300").should("have.length.greaterThan", 1);
+    cy.get("div.border-b.border-gray-300").should("have.length.greaterThan", 1)
+      .children()
+      .should('contain','Service Type') 
+      .and('contain',' Book Now ')
   });
 });
