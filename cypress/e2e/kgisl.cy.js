08f48eb59a274761cc5823ba4cc83e1fa580b295
sakirqa <sakir.qa@gmail.com>
2025-02-25 21:00:21 +0530

    Delete unnecessary comments

--- cypress/e2e/kgisl.cy.js
+++ cypress/e2e/kgisl.cy.js
@@ -1,40 +1,28 @@
-describe("KGiSL assessment",()=>{
+describe("KGiSL assessment", () => {
+  it("rate calculator", () => {
+    //Visit the pos site
+    cy.visit("https://www.pos.com.my/send/ratecalculator");
+    //Enter From postcode
+    cy.get('div.d-flex.mt-4>div>input[aria-label="Postcode"]')
+      .type("35600", { force: true })
+      .type("{enter}");
 
-    it("rate calculator",()=>{
-        cy.visit("https://www.pos.com.my/send/ratecalculator")
+    //Select India from To
+    cy.get('input[placeholder="Select country"]')
+      .click({ force: true })
+      .clear()
+      .type("India", { delay: 2000 })
+      .type("{downArrow}{enter}")
+      .invoke("val")
+      .should("eq", "India");
 
-        // cy.get("div.country-container>div.flex").scrollIntoView()
+    //Enter weight 1
+    cy.get('input[placeholder="eg. 0.1kg"]').click({ force: true }).type("1");
 
-        //Enter From postcode
-        cy.get('div.d-flex.mt-4>div>input[aria-label="Postcode"]')
-          .type("35600",{force:true})
-          .type("{enter}")  
+    //Click on Calculate
+    cy.contains(" Calculate ").click({ force: true });
 
-        //Select India from To
-        cy.get('input[placeholder="Select country"]')
-          .click({force:true})
-          .clear()
-          .type("India",{delay:2000})
-          .type('{downArrow}{enter}')
-          .invoke('val')
-          .should('eq','India')
-
-         //Enter weight 1
-         cy.get('input[placeholder="eg. 0.1kg"]')
-           .click({force:true})
-           .type('1')
-
-         //Click on Calculate
-         cy.contains(" Calculate ")
-           .click({force:true})
-
-        //Assert that multiple quotes return
-        cy.get("div.border-b.border-gray-300")
-          .should('have.length.greaterThan',1)
-
-    })
-
-
-
-
-})
+    //Assert that multiple quotes return
+    cy.get("div.border-b.border-gray-300").should("have.length.greaterThan", 1);
+  });
+});
